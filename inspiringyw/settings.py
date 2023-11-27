22e17b4607b3144e10a8bc45cdf66e932776aa90
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-11-27 21:13:41 +0000

    FIX: Update allowed hosts

--- inspiringyw/settings.py
+++ inspiringyw/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inspiring-young-writers.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'inspiring-young-writers.herokuapp.com', 'localhost']
 
 
 # Application definition
