dfc62956defe123b49aa2968b8e08a06e6be4c6f
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-11-29 11:22:14 +0000

    Prepare for deployment

--- inspiringyw/settings.py
+++ inspiringyw/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'inspiring-young-writers-0f3170377dc7.herokuapp.com', 'localhost']
 
