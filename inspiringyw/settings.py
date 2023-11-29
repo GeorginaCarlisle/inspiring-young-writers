de94ab0a40b2873bdf9bbc0f102f8f6de3ed1d16
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-11-29 11:17:00 +0000

    FIX: Incorrect and un-needed code

--- inspiringyw/settings.py
+++ inspiringyw/settings.py
@@ -140,12 +140,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-COMPRESS_ROOT = BASE_DIR / 'static'
-
-COMPRESS_ENABLED = True
-
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
