688c7734c4102cb914a2bfea6ed1d0d45dbd2378
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-12-12 11:35:47 +0000

    FEAT: My work title template to be used for all pages in my work area

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         './users/templates/login.html',
         './users/templates/logout.html',
         './writing/templates/create_writing.html',
+        './writing/templates/my_work_title.html',
     ],
     theme: {
         extend: {
