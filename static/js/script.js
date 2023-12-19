5ba511b6c58471334c1ef4b4a31cd391a99a438f
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-12-19 17:25:38 +0000

    FEAT: Aria hidden applied to all modals plus fixed JS bug in applying event listeners

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,8 @@
 // Load page before adding event listeners
 window.onload = function() {
+    // Call function to check if a close confirmation is needed
+    closeConfirmNeeded();
+
     // Add click event listener to the mobile nav hamburger
     let openNav = document.getElementById("nav-open");
     openNav.addEventListener('click', mobileNav);
@@ -12,30 +15,29 @@ window.onload = function() {
     let infoParentOpen = document.getElementById("info-parent-open");
     infoParentOpen.addEventListener('click', parentInfoMobile);
 
-    // Add click event listener to Parent info link in sign-up form
-    let openParentInfo = document.getElementById("open-parent-info");
-    openParentInfo.addEventListener('click', parentInfo);
-
-    // Call function to check if a close confirmation is needed
-    closeConfirmNeeded()
-    };
+    // Call function to check if html being loaded is sign-up form
+    inSignup();
+    }
   
 /**
  * Called when the hamburger is clicked. Click event Listener activated on load.
- * Mobile navigation displayed block and visible. 
- * Click event listener added to nav-closed button with anonymous function to hide modal.
+ * Mobile navigation displayed block and visible. Aria-hidden changed to false.
+ * Click event listener added to nav-closed button with anonymous function to hide modal
+ * and re-vert changes to style and aria-hidden.
  */
 function mobileNav() {
     // Display mobile navigation
     let navigation = document.getElementById("mobile-nav");
     navigation.style.display = "block";
+    navigation.setAttribute('aria-hidden', 'false');
     // Add click event listener to close symbol that re-hides modal
     let closeCross = document.getElementById("nav-closed");
     closeCross.onclick = function() {
         // Hide mobile nav
+        navigation.setAttribute('aria-hidden', 'true');
         navigation.style.display = "none";
-    };
-  }
+        };
+    }
 
 /**
  * Called when the 'Parent' button in the desktop nav or link from the sign-up form is clicked. 
@@ -55,8 +57,8 @@ function parentInfo() {
         // Hide parent information
         information.setAttribute('aria-hidden', 'true');
         information.style.display = "none";
-    };
-  }
+        };
+    }
 
 /**
  * Called when the 'Parent' button in the mobile nav is clicked. Click event Listener activated on load.
@@ -77,12 +79,12 @@ function parentInfoMobile() {
         // Hide parent information
         information.setAttribute('aria-hidden', 'true');
         information.style.display = "none";
-    };
-  }
+        };
+    }
 
 /**
  * Called on window loading
- * If there is a close button needing a confirmation message
+ * If there is a close button needing a confirmation message (found in create_writing.html and edit_writing.html)
  * a modal providing message and choice to go back or keep writing will
  * appear on clicking of the button
  */
@@ -94,12 +96,27 @@ function closeConfirmNeeded() {
             // display modal
             let confirmModal = document.getElementById("confirm-modal");
             confirmModal.style.display = "block";
+            confirmModal.setAttribute('aria-hidden', 'false');
             // event listener for keep writing
             let keepWriting = document.getElementById("keep-writing");
             keepWriting.addEventListener('click', function(){
                 // re-hide modal
                 confirmModal.style.display = "none";
-            })
-        });
+                confirmModal.setAttribute('aria-hidden', 'true');
+                })
+            });
+        }
+    }
+
+/**
+ * Called on window loading
+ * If the current page is signup.html and therefore the 'open-parent-info' can be found
+ * an event listener will be added to this element which on clicking will
+ * call the parentInfo function
+ */
+function inSignup() {
+    if (document.getElementById("open-parent-info")) {
+        let openParentInfo = document.getElementById("open-parent-info");
+        openParentInfo.addEventListener('click', parentInfo);
     }
 }
